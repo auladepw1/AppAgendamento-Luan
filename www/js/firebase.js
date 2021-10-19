a3d780f675fc914b980bc4c06e9f1fd01aa14db9
auladepw1 <emailparads@gmail.com>
2021-10-19 08:50:51 -0300

    a

---
+++ www/js/firebase.js
@@ -0,0 +1,17 @@
+//  Foi linkado ao firebase do Programador
+
+var firebaseConfig = {
+	
+	apiKey: "AIzaSyBYEh-0xdbmhvTVDa0tAoR--NCXOMvFI10",
+	authDomain: "app-agendamento-2ddfc.firebaseapp.com",
+	projectId: "app-agendamento-2ddfc",
+	storageBucket: "app-agendamento-2ddfc.appspot.com",
+	messagingSenderId: "423895487345",
+	appId: "1:423895487345:web:d44fd251062a80e5f55cd0",
+	measurementId: "G-L0G6C07YD2"
+	
+ };
+ 
+// Inicialização do Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
